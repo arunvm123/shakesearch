8879a5d5c5141b586ab410e6ec2de3d29541cc3f
Arun Mathew <arunvmathew95@gmail.com>
2021-01-01 16:09:59 +0530

    Made modification to seach backend

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"index/suffixarray"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"unicode"
 )
 
 func main() {
@@ -35,9 +36,16 @@ func main() {
 	}
 }
 
+type document struct {
+	ID   int
+	Text string
+}
+
+type index map[string][]int
+
 type Searcher struct {
-	CompleteWorks string
-	SuffixArray   *suffixarray.Index
+	CompleteWorks []document
+	Index         index
 }
 
 func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request) {
@@ -63,20 +71,147 @@ func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request
 }
 
 func (s *Searcher) Load(filename string) error {
-	dat, err := ioutil.ReadFile(filename)
+	f, err := os.Open(filename)
 	if err != nil {
-		return fmt.Errorf("Load: %w", err)
+		log.Fatal(err)
 	}
-	s.CompleteWorks = string(dat)
-	s.SuffixArray = suffixarray.New(dat)
+
+	scanner := bufio.NewScanner(f)
+
+	scanner.Split(bufio.ScanLines)
+
+	var documents []document
+
+	i := 0
+	for scanner.Scan() {
+		d := document{
+			ID:   i,
+			Text: scanner.Text(),
+		}
+		i = i + 1
+
+		documents = append(documents, d)
+	}
+
+	s.Index = make(index)
+
+	s.CompleteWorks = documents
+	s.Index.add(documents)
 	return nil
 }
 
 func (s *Searcher) Search(query string) []string {
-	idxs := s.SuffixArray.Lookup([]byte(query), -1)
+	idxs := s.Index.search(query)
 	results := []string{}
 	for _, idx := range idxs {
-		results = append(results, s.CompleteWorks[idx-250:idx+250])
+		results = append(results, s.Join(idx))
 	}
 	return results
+}
+
+func (s *Searcher) Join(idx int) string {
+	var res string
+	var lowerLimit, upperLimit int
+	if idx > 3 {
+		lowerLimit = idx - 3
+	}
+	if len(s.CompleteWorks)-idx < 3 {
+		upperLimit = len(s.CompleteWorks)
+	} else {
+		upperLimit = idx + 3
+	}
+
+	for i := lowerLimit; i < upperLimit; i++ {
+		res = res + s.CompleteWorks[i].Text
+	}
+
+	return res
+}
+
+func tokenize(text string) []string {
+	return strings.FieldsFunc(text, func(r rune) bool {
+		// Split on any character that is not a letter or a number.
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+	})
+}
+
+func lowercaseFilter(tokens []string) []string {
+	r := make([]string, len(tokens))
+	for i, token := range tokens {
+		r[i] = strings.ToLower(token)
+	}
+	return r
+}
+
+var stopwords = map[string]struct{}{
+	"a": {}, "and": {}, "be": {}, "have": {}, "i": {},
+	"in": {}, "of": {}, "that": {}, "the": {}, "to": {},
+}
+
+func stopwordFilter(tokens []string) []string {
+	r := make([]string, 0, len(tokens))
+	for _, token := range tokens {
+		if _, ok := stopwords[token]; !ok {
+			r = append(r, token)
+		}
+	}
+	return r
+}
+
+func analyze(text string) []string {
+	tokens := tokenize(text)
+	tokens = lowercaseFilter(tokens)
+	tokens = stopwordFilter(tokens)
+	return tokens
+}
+
+func (idx index) add(docs []document) {
+	for _, doc := range docs {
+		for _, token := range analyze(doc.Text) {
+			ids := idx[token]
+			if ids != nil && ids[len(ids)-1] == doc.ID {
+				// Don't add same ID twice.
+				continue
+			}
+			idx[token] = append(ids, doc.ID)
+		}
+	}
+}
+
+func intersection(a []int, b []int) []int {
+	maxLen := len(a)
+	if len(b) > maxLen {
+		maxLen = len(b)
+	}
+	r := make([]int, 0, maxLen)
+	var i, j int
+	for i < len(a) && j < len(b) {
+		if a[i] < b[j] {
+			i++
+		} else if a[i] > b[j] {
+			j++
+		} else {
+			r = append(r, a[i])
+			i++
+			j++
+		}
+	}
+	return r
+}
+
+func (idx index) search(text string) []int {
+	var r []int
+	for _, token := range analyze(text) {
+		if ids, ok := idx[token]; ok {
+			if r == nil {
+				r = ids
+			} else {
+				r = intersection(r, ids)
+			}
+		} else {
+			// Token doesn't exist.
+			return nil
+		}
+	}
+	return r
 }
